dcc3216e1dd0b5a82effc87cb1fe7e5c1214db3b
KTK27 <kartiksharmahkg852@gmail.com>
2024-08-02 14:37:34 +0800

    Start Features Section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import { Hero } from "@/components/ui/Hero";
 import { AllCardTypes } from "@/components/ui/all-card-types";
 import TestimonialCard from "@/components/ui/testomonial";
+import FeaturesSection from "@/components/ui/Features";
+
 
 export default function Home() {
   return (
     <><Hero />
       <AllCardTypes />
       <TestimonialCard />
+      <FeaturesSection />
     </>
   );
 }
